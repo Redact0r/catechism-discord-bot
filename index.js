f32667a8bf81f6206585c7245952f4a0e3217b86
Tyler Sharp <johntylersharp@gmail.com>
2021-02-24 23:47:25 -0500

    added user and reaction partials

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const bot = new Discord.Client();
+const bot = new Discord.Client({ partials: ["USER", "REACTION"] });
 bot.commands = new Discord.Collection();
 const botCommands = require("./commands/");
 const url = require("./sources/chanclalibrary");
