525480f8187bad760bfed3e8b35aed7f7f542b7e
Tyler Sharp <johntylersharp@gmail.com>
2021-02-27 22:32:54 -0500

    adding top5embed

--- commands/bonk.js
+++ commands/bonk.js
@@ -1,3 +1,4 @@
+const Discord = require("discord.js");
 const bonkService = require("../services/bonkService");
 
 function getUserFromMention(mention, users) {
@@ -20,9 +21,21 @@ module.exports = {
     const mention = args[1] == "check" || args[1] == "rank" ? args[2] : args[1];
 
     if (args[1] == "rank") {
-      console.log("sup");
       const top5 = await bonkService.getTop5Bonks();
-      return console.log(top5);
+      let n = 0;
+      const top5Embed = new Discord.MessageEmbed()
+        .setColor("#7851a9")
+        .setTitle("Bonk Leaderboard")
+        .addFields([
+          {
+            name: "Top 5 Bonk'd!",
+            value: top5.map((bonkedUser) => {
+              `${n++}. ${bonkedUser.used_id} ${bonkedUser.bonkCount}\n`;
+            }),
+          },
+        ]);
+
+      return msg.send(top5Embed);
     }
 
     const user = (await getUserFromMention(mention, client.users)) || null;
