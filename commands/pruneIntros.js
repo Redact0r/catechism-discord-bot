1c1a8c5606025ef508a062d322f4c3bc70bb157c
Noah K (BitForger) <bitforger.huyv0@8shield.net>
2025-04-27 16:40:44 -0400

    debug

--- commands/pruneIntros.js
+++ commands/pruneIntros.js
@@ -1,5 +1,17 @@
 const utils = require("../services/utils");
 
+function checkAndPruneMessage(message, users, logsChannel) {
+    // Check if the message is not from a bot
+    if (message.author.bot) return;
+    console.log(message.author)
+    console.log(message.author.id)
+    // Check if the message author is not a member of the server
+    if (!users.find((user) => user.id === message.author?.id)) {
+        logsChannel.send(`Pruning message from non-member: ${message.author.username} - ${utils.getMessageLink(message)}`);
+        // message.delete();
+    }
+}
+
 module.exports = {
     name: "!prune-intros",
     description: "Prune intros from the server",
@@ -34,20 +46,14 @@ module.exports = {
             channel.send("See logs in <#891742946859311114>")
             const maleMessages = await maleIntroChannel.messages.fetch()
             for (const message of maleMessages) {
-                if (!users.find((user) => user.id === message.author.id)) {
-                    logsChannel.send(`Pruning message from non-member: ${message.author.username} - ${utils.getMessageLink(message)}`);
-                    // message.delete();
-                }
+                checkAndPruneMessage(message, users, logsChannel);
             }
             mLoadMsg.edit("Removing messages from non-members in #introduction-male. <:CheckEmoji:1366143203857924116>")
 
             const fLoadMsg = await channel.send("Removing messages from non-members in #introductions-female. <a:BlurpleLoadEmoji:1366141437808345108>");
             const femaleMessages = await femaleIntroChannel.messages.fetch()
             for (const message of femaleMessages) {
-                if (!users.find((user) => user.id === message.author.id)) {
-                    logsChannel.send(`Pruning message from non-member: ${message.author.username} - ${utils.getMessageLink(message)}`);
-                    // message.delete();
-                }
+                checkAndPruneMessage(message, users, logsChannel);
             }
             fLoadMsg.edit("Removing messages from non-members in #introductions-female. <:CheckEmoji:1366143203857924116>")
 
