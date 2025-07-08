96794efd1a41b95f36a79501846fdb904f051755
Noah K (BitForger) <bitforger.huyv0@8shield.net>
2025-07-08 17:20:57 -0400

    fix: update edit command to support updated library

--- commands/edit.js
+++ commands/edit.js
@@ -3,7 +3,7 @@ import utils from "../services/utils.js";
 export default {
   name: "!edit",
   description: "let's a mod talk through the bot",
-  execute(msg, args, client) {
+  async execute(msg, args, client) {
     if (!utils.checkIfUserIsAuthorized(msg)) {
       msg.react("🚫");
       console.log("User is not authorized to use this command");
@@ -11,11 +11,24 @@ export default {
       console.log("User ID", msg.author.id);
       return
     }
-    if (args.length < 2) {
+    if (args.length < 3) {
       msg.reply(`Error, not enough arguments provided. Expected 2 arguments, received ${args.length}`);
       console.log("Error, not enough arguments provided. Expected 2 arguments, received", args.length);
       console.log("Arguments", args);
       return;
+    }
+
+    let channelID = args.shift();
+    if (channelID.startsWith("<#")) {
+      // Extract channel ID from <#channel_id>
+      channelID = args[0].substring(2, args[0].length - 1);
+    } else {
+      // If the first argument is not a mention, it should be a channel ID
+      if (isNaN(parseInt(channelID))) {
+        msg.reply("Error, first argument must be a channel ID or a mention.");
+        console.log("Error, first argument must be a channel ID or a mention.");
+        return;
+      }
     }
 
     let messageID = args.shift();
@@ -23,31 +36,35 @@ export default {
       // Extract message ID from <#message_id>
       messageID = args[0].substring(2, args[0].length - 1);
     } else {
-        // If the first argument is not a mention, it should be a message ID
-        if (isNaN(parseInt(messageID))) {
-            msg.reply("Error, first argument must be a message ID or a mention.");
-            console.log("Error, first argument must be a message ID or a mention.");
-            return;
-        }
+      // If the second argument is not a mention, it should be a message ID
+      if (isNaN(parseInt(messageID))) {
+        msg.reply("Error, second argument must be a message ID or a mention.");
+        console.log("Error, second argument must be a message ID or a mention.");
+        return;
+      }
     }
 
-    const messageText = args.join(" ");
+    const messageText = msg.content.split(" ").slice(2).join(" ");
 
     console.log("Sending message to channel:", messageID);
     console.log("Message text:", messageText);
-    if (!isNaN(parseInt(messageID))) {
-      msg.channel.messages.fetch(messageID).then((m) => {
-        m.edit(messageText).catch((err) => {
-          console.error(err);
-          if (err.status === 404) {
-            msg.reply("Error, message not found.");
-            msg.reply("This could be because the message was not found or the bot does not have permission to edit the message.");
-          } else if (err.status === 403) {
-            msg.reply("Error, bot does not have permission to edit the message.");
-          }
-        });
-      });
-      msg.delete();
+    const channel = client.channels.cache.get(channelID);
+    if (!channel) {
+      msg.reply("Error, channel not found.");
+      console.log("Error, channel not found. Channel ID:", channelID);
+      return;
     }
-  },
+
+    const message = await channel.messages.fetch(messageText)
+    message.edit(messageText).catch((err) => {
+      console.error(err);
+      if (err.status === 404) {
+        msg.reply("Error, message not found.");
+        msg.reply("This could be because the message was not found or the bot does not have permission to edit the message.");
+      } else if (err.status === 403) {
+        msg.reply("Error, bot does not have permission to edit the message.");
+      }
+    });
+    msg.react("✅");
+  }
 };
