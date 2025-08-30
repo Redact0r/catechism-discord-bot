d1c9c0df26ceba153db47aa2b807bf73cb61a965
Noah K (BitForger) <bitforger.huyv0@8shield.net>
2025-08-29 20:44:12 -0400

    chore: log formatting

--- commands/edit.js
+++ commands/edit.js
@@ -6,15 +6,15 @@ export default {
     async execute(msg, args, client) {
         if (!utils.checkIfUserIsAuthorized(msg)) {
             msg.react("🚫");
-            console.log("User is not authorized to use this command");
-            console.log("User", msg.author.username);
-            console.log("User ID", msg.author.id);
+            console.debug("[DEBUG] User is not authorized to use this command");
+            console.debug("[DEBUG] User", msg.author.username);
+            console.debug("[DEBUG] User ID", msg.author.id);
             return
         }
         if (args.length < 3) {
             msg.reply(`Error, not enough arguments provided. Expected 2 arguments, received ${args.length}`);
-            console.log("Error, not enough arguments provided. Expected 2 arguments, received", args.length);
-            console.log("Arguments", args);
+            console.error("[ERROR] Error, not enough arguments provided. Expected 2 arguments, received", args.length);
+            console.debug("[DEBUG] Arguments", args);
             return;
         }
 
@@ -26,7 +26,7 @@ export default {
             // If the first argument is not a mention, it should be a channel ID
             if (isNaN(parseInt(channelID))) {
                 msg.reply("Error, first argument must be a channel ID or a mention.");
-                console.log("Error, first argument must be a channel ID or a mention.");
+                console.error("[ERROR] Error, first argument must be a channel ID or a mention.");
                 return;
             }
         }
@@ -39,7 +39,7 @@ export default {
             // If the second argument is not a mention, it should be a message ID
             if (isNaN(parseInt(messageID))) {
                 msg.reply("Error, second argument must be a message ID or a mention.");
-                console.log("Error, second argument must be a message ID or a mention.");
+                console.error("[ERROR] Error, second argument must be a message ID or a mention.");
                 return;
             }
         }
@@ -50,12 +50,12 @@ export default {
             return text.substring(splitText.length);
         })(msg.content)
 
-        console.log("Sending message to channel:", messageID);
-        console.log("Message text:", messageText);
+        console.debug("[DEBUG] Sending message to channel:", messageID);
+        console.debug("[DEBUG] Message text:", messageText);
         const channel = client.channels.cache.get(channelID);
         if (!channel) {
             msg.reply("Error, channel not found.");
-            console.log("Error, channel not found. Channel ID:", channelID);
+            console.error("[ERROR] Error, channel not found. Channel ID:", channelID);
             return;
         }
 
