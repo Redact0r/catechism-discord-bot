5b9ce85544eb6ea7c8738f70067969cc6c8897f4
Noah K (BitForger) <bitforger.huyv0@8shield.net>
2025-11-30 22:01:18 -0500

    feat: update verification command to include subcommand for sending instructions

--- commands/slash/get-verified.js
+++ commands/slash/get-verified.js
@@ -2,22 +2,33 @@ import {SlashCommandBuilder} from "discord.js";
 import {getVerificationInstructions} from "../../services/utils.js";
 
 export default {
+    name: "verify",
     enabledInProd: false,
     data: new SlashCommandBuilder()
-        .setName("get-verified")
-        .setDescription("Get the verified role")
-        .addChannelOption(input =>
-            input.setName("channel")
-                .setDescription("The channel to send the verification message to")
-                .setRequired(true)
-        )
-        .addUserOption(user =>
-            user.setName("user")
-                .setDescription("The user to get verified")
-                .setRequired(true)
-        ),
+        .setName("verify")
+        .setDescription("Verification helper command")
+        .addSubcommand(subCommand =>
+            subCommand
+                .setName("send-instructions")
+                .setDescription("Send verification instructions to a channel")
+                .addChannelOption(input =>
+                    input.setName("channel")
+                        .setDescription("The channel to send the verification message to")
+                        .setRequired(true)
+                )
+                .addUserOption(user =>
+                    user.setName("user")
+                        .setDescription("The user to get verified")
+                        .setRequired(true)
+                )),
     async execute(interaction) {
-        const member = interaction.member;
+        const subCommand = interaction.options.getSubcommand();
+
+        if (subCommand === "send-instructions") {
+            return await this.sendVerificationInstructions(interaction);
+        }
+    },
+    async sendVerificationInstructions(interaction) {
         const channel = interaction.options.getChannel("channel")
         const user = interaction.options.getUser("user")
         await channel.send(getVerificationInstructions(user, user.username))
